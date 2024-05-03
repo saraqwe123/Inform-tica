e5549a4ec88f1e0b8012fdb46001320564fb61d6
Sara Guaiume <130059301+saraqwe123@users.noreply.github.com>
2024-05-03 16:08:37 -0300

    03/05
    finalizando a função do js que verifica se os campos estão preenchidos corretamente

--- cadastro/javascript1.js
+++ cadastro/javascript1.js
@@ -12,5 +12,27 @@ function menu(){
 }
 
 function cadastro() {
-    alert('Cadastro realizado com sucesso!')
+    var cpf = document.getElementById("icpf").value
+    var rg = document.getElementById("irg").value
+    var email = document.getElementById("iemail").value
+    var resp_produto = document.getElementById("iproduto").value
+    var nome = document.getElementById("inome").value
+    var senha = document.getElementById("isenha").value
+
+    let inputs = [cpf, rg, email, resp_produto, nome, senha]
+
+    let i = 0
+    for (let input  of inputs) {
+        if (input != "" && email.indexOf("@") != -1 && senha.lenght >= 5) {
+            i += ""
+        } else {
+            i += 1
+        }
+    }
+    
+    if (i != "") {
+        alert("Preencha os campos corretamente!")
+    } else {
+        alert("Cadastro realizado com sucesso!")
+    }
 }
