d384a892335e91e0d45a5c92fbf2574aa82041b5
Sara Guaiume <130059301+saraqwe123@users.noreply.github.com>
2024-05-02 21:54:56 -0300

    Reapply "02/05"
    This reverts commit 51be37c6a0265e608912fcee817c1972dd2a3890.

--- login/javascript.js
+++ login/javascript.js
@@ -1,7 +1,6 @@
 function menu(){
     var menu = document.getElementById('menu')
     var botao = document.getElementById('botao')
-
     
     if (menu.style.display === "none") {
         botao.style.display = "none"
@@ -13,18 +12,13 @@ function menu(){
 }
 
 function cadastro() {
+
     var nome = document.getElementById('inome')
     var senha = document.getElementById('isenha')
-    var cpf = document.getElementById('icpf')
-    var email =document.getElementById('iemail')
-    var premium1 = document.getElementById('ipremium')
-    var premium2 = document.getElementById('premium')
 
-    if (nome != "" && senha != "" && cpf != "" && email != "")  {
-        if (premium1 !="" || premium2 != "") {
-            alert('Cadastro realizado com sucesso1')
-        }
+    if (nome != "" && senha !="") {
+        alert('Login realizado com sucesso1')
     } else {
-        alert('Não foi possível realizar o cadastro')
+        alert('Não foi possível realizar o login')
     }
 }
