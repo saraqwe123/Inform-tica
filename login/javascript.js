8f6ee2f8ce0abf083c0073a3707da420abfe28c5
Sara Guaiume <130059301+saraqwe123@users.noreply.github.com>
2024-05-05 16:52:20 -0300

    05/05

--- login/javascript.js
+++ login/javascript.js
@@ -1,12 +1,36 @@
-function menu(){
-    var menu = document.getElementById('menu')
-    var botao = document.getElementById('botao')
-    
-    if (menu.style.display === "none") {
-        botao.style.display = "none"
-        menu.style.display = "block"
+const menu = document.getElementById('menu');
+
+function adc_menu(event) {
+    console.log(event.target);
+    console.log("adc_menu function called");
+    if (!menu.contains(event.target) && event.target.id !== 'botao') {
+        menu.classList.remove("aparecer");
+        //!menu.contains ve se esta fora do menu, se nao tivesse a ! veria se esta dentro//
     } else {
-        botao.style.display = "block"
-        menu.style.display = "none"
+        menu.classList.toggle("aparecer");
+    }
+}
+
+document.addEventListener("click", adc_menu);
+
+function cadastro() {
+    var nome = document.getElementById("inome").value
+    var senha = document.getElementById("isenha").value
+
+    let inputs = [nome, senha]
+
+    let i = 0
+    for (let input  of inputs) {
+        if (input !== "" && senha.length >= 5) {
+            i += 0
+        } else {
+            i += 1
+        }
+    }   
+
+    if (i != 0) {
+        alert("Preencha os campos corretamente!")
+    } else {
+        alert("Cadastro realizado com sucesso!")
     }
 }
