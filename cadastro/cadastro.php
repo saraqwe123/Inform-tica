b3a93a1ab27323dfa151ba9b803970cf35701074
Sara Guaiume <130059301+saraqwe123@users.noreply.github.com>
2024-05-17 11:44:39 -0300

    Update cadastro.php

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -24,28 +24,28 @@
         <form action="">
             <label for="icpf">CPF</label>
             <br>
-            <input type="text" name="" id="icpf" required size="35" style="background-color: #F1F3F6">
+            <input type="text" name="cpf" id="icpf" required size="35" style="background-color: #F1F3F6">
             <br>
             <label for="irg">RG</label>
             <br>
-            <input type="text" name="" id="irg" required size="35" style="background-color: #F1F3F6">
+            <input type="text" name="rg" id="irg" required size="35" style="background-color: #F1F3F6">
             <br>
             <label for="iemail">Email</label>
             <br>
-            <input type="email" name="" id="iemail" required size="35" style="background-color: #F1F3F6">
+            <input type="email" name="email" id="iemail" required size="35" style="background-color: #F1F3F6">
             <br>
             <label for="inome">Nome User</label>
             <br>
-            <input type="text" name="" id="inome" required size="35" style="background-color: #F1F3F6">
+            <input type="text" name="nome" id="inome" required size="35" style="background-color: #F1F3F6">
             <br>
             <label for="isenha">Senha User</label>
             <br>
-            <input type="password" name="" id="isenha" required minlength="5" placeholder="5 digitos" size="35"
+            <input type="password" name="senha" id="isenha" required minlength="5" placeholder="5 digitos" size="35"
                 style="background-color: #F1F3F6">
             <br>
             <label for="">Chave?</label>
             <br>
-            <input type="text" name="" id="ipremium" placeholder="Para usuários supremos" required size="35" style="background-color: #F1F3F6">
+            <input type="text" name="premium" id="ipremium" placeholder="Para usuários supremos" required size="35" style="background-color: #F1F3F6">
             <br>
             <input class="btn" type="submit" onclick="cadastro()">
         </form>
@@ -60,11 +60,12 @@ if(isset($_POST['submit'])) {
 
     $cpf = $_POST['cpf'];
     $rg = $_POST['rg'];
+    $email = $_POST['email']
     $nome = $_POST['nome'];
     $senha = $_POST['senha'];
     $premium = $_POST['premium'];
 
-    $usuarios = mysqli_query($conexao, "INSERT INTO cadastro_usuarios(cpf, rg, email, nome_usuario, senha_usuario, chave) VALUES('$cpf', '$rg', '$nome', '$senha', '$premium')");
+    $usuarios = mysqli_query($conexao, "INSERT INTO cadastro_usuarios(cpf, rg, email, nome_usuario, senha_usuario, chave) VALUES('$cpf', '$rg', '$email', '$nome', '$senha', '$premium')");
 
 
 
